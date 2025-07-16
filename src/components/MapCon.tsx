dd122d7da1c078e05f37e5de6287dc427793690a
Jethro <balagajet@gmail.com>
2025-07-16 20:55:29 +0800

    Map error handling

--- src/components/MapCon.tsx
+++ src/components/MapCon.tsx
@@ -3,16 +3,18 @@ import 'leaflet/dist/leaflet.css';
 
 const MapCon: React.FC = () => {
   return (
-    <MapContainer
-      style={{ height: "100vh", width: "100%" }}
-      center={[8.35985, 124.869077] as [number, number]}
-      scrollWheelZoom={false} 
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <MapContainer
+        center={[8.35985, 124.869077]}
+        zoom={18}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
   );
 };
 
