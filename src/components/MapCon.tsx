01903f87b41998e6fe33bd4921b57e2820556d9a
Jethro <balagajet@gmail.com>
2025-07-16 20:49:43 +0800

    npm install leaflet react-leaflet @types/leaflet

--- src/components/MapCon.tsx
+++ src/components/MapCon.tsx
@@ -1,11 +1,12 @@
 import { MapContainer, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 
 const MapCon: React.FC = () => {
   return (
     <MapContainer
       style={{ height: "100vh", width: "100%" }}
-      center={[8.35985, 124.869077]}
-      zoom={18}
+      center={[8.35985, 124.869077] as [number, number]}
+      scrollWheelZoom={false} 
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
