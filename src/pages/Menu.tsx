c0e40955f81f637e06c9dbfbb1b782cb7782bb73
Jethro <balagajet@gmail.com>
2025-07-17 22:27:17 +0800

    Added menu.tsx and added it to route.tsx, and Map.tsx is added in menu route

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,66 @@
+import {
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonMenu,
+    IonMenuButton,
+    IonMenuToggle,
+    IonPage,
+    IonRouterOutlet,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react'
+import { mapOutline } from 'ionicons/icons';
+import { Redirect, Route } from 'react-router';
+import Map from './Map';
+const Menu: React.FC = () => {
+    const path = [
+        { name: 'Map', url: '/map', icon: mapOutline},
+    ]
+
+    return (
+        <>
+            <IonMenu contentId="main-content">
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Menu Content</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    {path.map((item, index) => (
+                        <IonMenuToggle key={index}>
+                            <IonItem routerLink={item.url} routerDirection="forward">
+                                <IonIcon icon={item.icon} slot="start"></IonIcon>
+                                {item.name}
+                            </IonItem>
+                        </IonMenuToggle>
+                    ))}
+                </IonContent>
+            </IonMenu>
+            <IonPage id="main-content">
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonMenuButton></IonMenuButton>
+                        </IonButtons>
+                        <IonTitle>Menu</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    <IonRouterOutlet id="main">
+                        <Route exact path="/map" component={Map} />
+
+                        <Route exact path="/">
+                            <Redirect to="/map" />
+                        </Route>
+                    </IonRouterOutlet>
+                </IonContent>
+            </IonPage>
+        </>
+    );
+};
+
+export default Menu;
