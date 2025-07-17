e85294c2119e4c78479c4bea32f99dbdd94987f3
Jethro <balagajet@gmail.com>
2025-07-17 23:02:16 +0800

    Updated HouseNumberInput to use HouseNumberInputProps

--- src/components/GeoTaggingComponents/HouseNumberInput.tsx
+++ src/components/GeoTaggingComponents/HouseNumberInput.tsx
@@ -1,12 +1,21 @@
-// HouseNumberInput.tsx
 import React from 'react';
 import { IonItem, IonLabel, IonInput } from '@ionic/react';
 
-const HouseNumberInput: React.FC = () => {
+interface HouseNumberInputProps {
+  value: number;
+  onChange: (value: number) => void;
+}
+
+const HouseNumberInput: React.FC<HouseNumberInputProps> = ({ value, onChange }) => {
   return (
     <IonItem>
       <IonLabel position="floating">House Number</IonLabel>
-      <IonInput type="number" placeholder="Enter house number" />
+      <IonInput 
+        type="number" 
+        placeholder="Enter house number"
+        value={value}
+        onIonChange={(e) => onChange(Number(e.detail.value!))}
+      />
     </IonItem>
   );
 };
