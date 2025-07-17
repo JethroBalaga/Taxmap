11a4f704726a8be82c642600a9a60281c59b323e
Jethro <balagajet@gmail.com>
2025-07-17 23:30:13 +0800

    Change Marker Size

--- src/utils/markerIcons.tsx
+++ src/utils/markerIcons.tsx
@@ -5,7 +5,7 @@ import blueMarkerIconUrl from '../assets/Blue.png';
 export const createBlueMarkerIcon = () => {
   return L.icon({
     iconUrl: blueMarkerIconUrl,
-    iconSize: [25, 41],
+    iconSize: [45, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
