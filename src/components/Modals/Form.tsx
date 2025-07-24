daf1a7c57d64749d4d1fd945058b66c41b3bcf3c
Jethro <balagajet@gmail.com>
2025-07-24 18:52:38 +0800

    Minor form fixes

--- src/components/Modals/Form.tsx
+++ src/components/Modals/Form.tsx
@@ -1,25 +1,48 @@
 import React, { useState } from 'react';
-import { IonContent } from '@ionic/react';
+import {
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonButtons
+} from '@ionic/react';
 import Declarant from '../FormComponents/Declarant';
 import Kind from '../FormComponents/Kind';
 import Classification from '../FormComponents/Classification';
 import Area from '../FormComponents/Area';
 import Next from '../GlobalComponent/Next';
 
-const Form: React.FC = () => {
+interface FormProps {
+  isOpen: boolean;
+  onDismiss: () => void;
+}
+
+const Form: React.FC<FormProps> = ({ isOpen, onDismiss }) => {
   const [declarant, setDeclarant] = useState('');
   const [kind, setKind] = useState('');
   const [classification, setClassification] = useState('');
   const [area, setArea] = useState<number>(0);
 
   return (
-    <IonContent className="ion-padding">
-      <Declarant value={declarant} onChange={setDeclarant} />
-      <Kind value={kind} onChange={setKind} />
-      <Classification value={classification} onChange={setClassification} />
-      <Area value={area} onChange={setArea} />
-      <Next />
-    </IonContent>
+    <IonModal isOpen={isOpen} onDidDismiss={onDismiss}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tag New Location</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={onDismiss}>Close</IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <Declarant value={declarant} onChange={setDeclarant} />
+        <Kind value={kind} onChange={setKind} />
+        <Classification value={classification} onChange={setClassification} />
+        <Area value={area} onChange={setArea} />
+        <Next />
+      </IonContent>
+    </IonModal>
   );
 };
 
