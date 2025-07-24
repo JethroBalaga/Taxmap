162c983d03a07527c9c931bcc258ea5350cf6b72
Jethro <balagajet@gmail.com>
2025-07-24 18:36:28 +0800

    Added Next.tsx in globalcomponent folder

---
+++ src/components/GlobalComponent/Next.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IonButton } from '@ionic/react';
+
+const Next: React.FC = () => {
+  return (
+    <IonButton expand="block" color="primary">
+      Next
+    </IonButton>
+  );
+};
+
+export default Next;
