815440a83d4a9a8db3fe37ae20f63bff533ed45d
Jethro <balagajet@gmail.com>
2025-08-07 00:55:18 +0800

    Map changes on offline usage

--- src/components/MapCon.tsx
+++ src/components/MapCon.tsx
@@ -3,70 +3,36 @@ import React, { useEffect, useRef, useState } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet.offline';
+import 'leaflet.offline'; // offline plugin
 import localforage from 'localforage';
 import Form from './Modals/Form';
 
-// Constants
+const TILE_LAYER_URL = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
 const manoloFortichBounds = L.latLngBounds(
   L.latLng(8.25, 124.75),
   L.latLng(8.45, 124.95)
 );
+
 const DEFAULT_ZOOM = 14;
 const MIN_ZOOM_LOCKED = 14;
 const MIN_ZOOM_UNLOCKED = 12;
 const MAX_ZOOM = 18;
-const TILE_LAYER_URL = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
 
-const CREATE_OUTLINE_PATH = "M384 224v184a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V168a40 40 0 0 1 40-40h167.48M336 64h112v112M224 288L440 72";
-
-// Create Button
-const CreateOutlineControl = ({ onClick }: { onClick: () => void }) => {
-  const map = useMap();
-  const controlRef = useRef<any>(null);
-
-  useEffect(() => {
-    const CustomControl = L.Control.extend({
-      options: { position: 'topright' },
-      onAdd: function () {
-        const container = L.DomUtil.create('div', 'leaflet-control');
-        const link = L.DomUtil.create('a', 'create-outline-btn', container);
-        link.href = '#';
-        link.title = 'Create New Location';
-        link.innerHTML = `
-          <svg viewBox="0 0 512 512" width="20" height="20">
-            <path fill="currentColor" d="${CREATE_OUTLINE_PATH}"/>
-          </svg>
-        `;
-        L.DomEvent.on(link, 'click', (e) => {
-          L.DomEvent.stop(e);
-          onClick();
-        });
-        return container;
-      }
-    });
-
-    controlRef.current = new CustomControl();
-    controlRef.current.addTo(map);
-    return () => controlRef.current?.remove();
-  }, [map, onClick]);
-
-  return null;
-};
-
-// Map Controls
-const MapController = ({ onOpenForm }: { onOpenForm: () => void }) => {
+// Component to set up the map logic
+const MapLogic = () => {
   const map = useMap();
   const tileLayerRef = useRef<any>(null);
   const [allowZoomOut, setAllowZoomOut] = useState(false);
 
-  const initOfflineLayer = () => {
+  useEffect(() => {
+    // LocalForage config
     localforage.config({
       driver: [localforage.INDEXEDDB, localforage.WEBSQL, localforage.LOCALSTORAGE],
       name: 'ManoloFortichMap',
       storeName: 'map_tiles'
     });
 
+    // Add Offline Tile Layer
     const offlineLayer = (L.tileLayer as any).offline(TILE_LAYER_URL, {
       attribution: 'Tiles &copy; Esri &mdash; Source: Esri, Maxar, Earthstar Geographics',
       noWrap: true,
@@ -74,30 +40,36 @@ const MapController = ({ onOpenForm }: { onOpenForm: () => void }) => {
       maxZoom: MAX_ZOOM
     });
 
-    offlineLayer.on('tileloadend', (event: any) => {
-      if (offlineLayer.saveTile) offlineLayer.saveTile(event.tile);
+    offlineLayer.on('tileloadend', (e: any) => {
+      if (offlineLayer.saveTile) {
+        offlineLayer.saveTile(e.tile);
+      }
     });
 
     offlineLayer.addTo(map);
     tileLayerRef.current = offlineLayer;
-    return () => offlineLayer.remove();
-  };
 
-  const enforceRestrictions = () => {
-    const currentZoom = map.getZoom();
-    if (currentZoom > DEFAULT_ZOOM) setAllowZoomOut(true);
-    if (!allowZoomOut && currentZoom < DEFAULT_ZOOM) map.setZoom(DEFAULT_ZOOM);
-    else if (allowZoomOut && currentZoom < MIN_ZOOM_UNLOCKED) map.setZoom(MIN_ZOOM_UNLOCKED);
-    if (!manoloFortichBounds.contains(map.getCenter())) {
-      map.panInsideBounds(manoloFortichBounds, { animate: false });
-    }
-  };
-
-  useEffect(() => {
+    // Initial map view and restriction
     map.setView([8.35985, 124.869077], DEFAULT_ZOOM);
     map.setMaxBounds(manoloFortichBounds);
 
-    const cleanup = initOfflineLayer();
+    const enforceRestrictions = () => {
+      const currentZoom = map.getZoom();
+
+      if (currentZoom > DEFAULT_ZOOM) {
+        setAllowZoomOut(true);
+      }
+
+      if (!allowZoomOut && currentZoom < DEFAULT_ZOOM) {
+        map.setZoom(DEFAULT_ZOOM);
+      } else if (allowZoomOut && currentZoom < MIN_ZOOM_UNLOCKED) {
+        map.setZoom(MIN_ZOOM_UNLOCKED);
+      }
+
+      if (!manoloFortichBounds.contains(map.getCenter())) {
+        map.panInsideBounds(manoloFortichBounds, { animate: false });
+      }
+    };
 
     map.on('zoomend', enforceRestrictions);
     map.on('move', enforceRestrictions);
@@ -107,11 +79,11 @@ const MapController = ({ onOpenForm }: { onOpenForm: () => void }) => {
     return () => {
       map.off('zoomend', enforceRestrictions);
       map.off('move', enforceRestrictions);
-      cleanup();
+      offlineLayer.remove();
     };
   }, [map, allowZoomOut]);
 
-  return <CreateOutlineControl onClick={onOpenForm} />;
+  return null;
 };
 
 // Main Map Component
@@ -126,24 +98,6 @@ const MapCon: React.FC = () => {
 
   return (
     <div style={{ height: '100vh', width: '100%', overflow: 'hidden', position: 'relative' }}>
-      <style>{`
-        .create-outline-btn {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          width: 30px;
-          height: 30px;
-          background: white;
-          border-radius: 50%;
-          box-shadow: 0 1px 5px rgba(0,0,0,0.4);
-          cursor: pointer;
-        }
-        .create-outline-btn:hover {
-          background: #f4f4f4;
-          transform: scale(1.1);
-        }
-      `}</style>
-
       {isMounted && (
         <>
           <MapContainer
@@ -155,11 +109,12 @@ const MapCon: React.FC = () => {
             maxBounds={manoloFortichBounds}
             maxBoundsViscosity={1.0}
           >
+            {/* This only renders the base layer visually (non-offline) */}
             <TileLayer
               url={TILE_LAYER_URL}
               attribution='Tiles &copy; Esri &mdash; Source: Esri, Maxar, Earthstar Geographics'
             />
-            <MapController onOpenForm={() => setShowForm(true)} />
+            <MapLogic />
           </MapContainer>
 
           <Form
