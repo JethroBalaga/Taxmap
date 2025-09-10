021b38eef22dfe2b96f8e0a3acdce0410bf1ff96
Jethro <balagajet@gmail.com>
2025-09-10 19:34:45 +0800

    Added BuildingTable tab

---
+++ src/pages/Formstabs/BuildingTable.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from "@ionic/react";
+
+const BuildingTable: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Building Table</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent className="ion-padding">
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default BuildingTable;
