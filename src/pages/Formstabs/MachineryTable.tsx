eae54b4a061e0fcf2627a9daf1777e504d59ec2b
Jethro <balagajet@gmail.com>
2025-10-05 21:10:26 +0800

    Created MachineryTable.tsx

---
+++ src/pages/Formstabs/MachineryTable.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+
+const MachineryTable: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Machinery Table</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+};
+
+export default MachineryTable;
