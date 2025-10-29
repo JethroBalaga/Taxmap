25dbb9a12ad5a54bb36326d66021758126a4ffbb
Jethro <balagajet@gmail.com>
2025-10-29 19:17:48 +0800

    Add agricultural data support to StorageUtils

--- src/utils/PageUtils/StorageUtils.ts
+++ src/utils/PageUtils/StorageUtils.ts
@@ -80,6 +80,8 @@ export const calculateStorageUsage = async (
               displayValue = `${value.length} value info records`;
             } else if (key === 'photoTags' && Array.isArray(value)) {
               displayValue = `${value.length} photo tag records`;
+            } else if (key === 'agriculturalData' && Array.isArray(value)) { // Add this condition
+              displayValue = `${value.length} agricultural data records`;
             } else if (typeof value === 'object') {
               displayValue = 'Object data';
             } else {
